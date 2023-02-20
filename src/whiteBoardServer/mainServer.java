6e7eaa1c27d0b4849de95393263ca76c34e943ab
YiyuanWei <yiyuanw1@student.unimelb.edu.au>
2023-02-21 10:19:40 +1100

    build project with maven

--- src/whiteBoardServer/mainServer.java
+++ src/whiteBoardServer/mainServer.java
@@ -38,75 +38,73 @@ public class mainServer {
 	public static ServerLoginPage login;
 	public static int port;
 	public static Object loginLock = new Object();
-	
+
 	private static String IPS;
 	private static String portS;
 
-public static void main(String[] args) {
-	
-	login = new ServerLoginPage();
-	login.pack();
+	public static void main(String[] args) {
 
-	synchronized(loginLock) {
-		try {
-			while(login.getLoginIp() == null &&  login.getLoginPort() == null && login.getLoginName() == null) {
-				loginLock.wait();
-				IPS = login.getLoginIp();
-				portS = login.getLoginPort();
-				userName = login.getLoginName();
+		login = new ServerLoginPage();
+		login.pack();
+
+		synchronized (loginLock) {
+			try {
+				while (login.getLoginIp() == null && login.getLoginPort() == null && login.getLoginName() == null) {
+					loginLock.wait();
+					IPS = login.getLoginIp();
+					portS = login.getLoginPort();
+					userName = login.getLoginName();
+				}
+			} catch (InterruptedException e1) {
+				System.out.println(e1.getMessage());
 			}
-		} catch (InterruptedException e1) {
-			System.out.println(e1.getMessage());
 		}
-	}
 
-	f = new ManagerCanvas();
-	f.initCanvas();
-    f.pack();
-    f.setVisible(true);
-    new UserPool();
-    UserPool.newUser(userName + "(manager)");
-    
-	try {
-		IP = InetAddress.getByName(IPS);
-		port = Integer.parseInt(portS);
-		wbs = new WhiteBoardServer(userName, IP, port, 10);
-	} catch (ServerException | IOException | InterruptedException e2) {
-		System.out.println(e2.getMessage());
-	}
-	
-    f.addWindowListener(new WindowAdapter() {
-        @Override
-        public void windowClosing(WindowEvent e) {
+		f = new ManagerCanvas();
+		f.initCanvas();
+		f.pack();
+		f.setVisible(true);
+		new UserPool();
+		UserPool.newUser(userName + "(manager)");
 
-            f.getService().saveAs(false,f);
-            e.getWindow().dispose();
-            
-            int socketNum = ServerListeningSocketThread.threadMaster.size();
-            for(int i = 0; i < socketNum; i++) {
-            	Socket skt = ServerListeningSocketThread.threadMaster.get(i).getSocket();
-            	Thread thd = ServerListeningSocketThread.threadMaster.get(i).getThread();
-            	try {
-					skt.shutdownInput();
-					skt.shutdownOutput();
-					skt.close();
-					thd.interrupt();
-				} catch (IOException e1) {
+		try {
+			IP = InetAddress.getByName(IPS);
+			port = Integer.parseInt(portS);
+			wbs = new WhiteBoardServer(userName, IP, port, 10);
+		} catch (ServerException | IOException | InterruptedException e2) {
+			System.out.println(e2.getMessage());
+		}
+
+		f.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+
+				f.getService().saveAs(false, f);
+				e.getWindow().dispose();
+
+				int socketNum = ServerListeningSocketThread.threadMaster.size();
+				for (int i = 0; i < socketNum; i++) {
+					Socket skt = ServerListeningSocketThread.threadMaster.get(i).getSocket();
+					Thread thd = ServerListeningSocketThread.threadMaster.get(i).getThread();
+					try {
+						skt.shutdownInput();
+						skt.shutdownOutput();
+						skt.close();
+						thd.interrupt();
+					} catch (IOException e1) {
+					}
+
+					try {
+						wbs.getLiseningSocket().close();
+						wbs.getServerListeningSocketThread().interrupt();
+					} catch (IOException e1) {
+					}
+
 				}
-            	
-            	try {
-					wbs.getLiseningSocket().close();
-					wbs.getServerListeningSocketThread().interrupt();
-				} catch (IOException e1) {
-				}
-            	
-            }
-            
-            
-        }
-    });
 
-}
+			}
+		});
 
+	}
 
 }
